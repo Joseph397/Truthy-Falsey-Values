48b78415c4b0c79136291b2a33d0b3a989f77be0
name <gramkracker88@gmail.com>
2020-09-12 13:45:16 -0700

    first commit

---
+++ Truthy.js
@@ -0,0 +1,32 @@
+// Truthy Falsey Values
+
+// evaluates to truthy
+// const age = 25;
+
+// if (age){
+//     console.log('This value is truthy');
+// } else {
+//     console.log('This value is falsey');
+// }
+
+// //ex2:- evaluates to truthy
+// const age = 25;
+
+// if (true){
+//     console.log('This value is truthy');
+// } else {
+//     console.log('This value is falsey');
+// }
+
+//ex3:- evaluates to falsey
+const age = 25;
+
+if (false){
+    console.log('This value is truthy');
+} else {
+    console.log('This value is falsey');
+}
+
+// all Falsey values
+// false, 0, "", null, undefined, Nan
+
